bba448bfbd1d14ad5241486ade59a5a5ae067225
Beth Douglas <bethDdouglas@gmail.com>
2017-10-25 14:10:09 -0400

    added Conditionals

---
+++ 0.04_Conditionals/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._04_Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How are you feeling from 1-5?");
+            string feelingNumber = Console.ReadLine();
+            if (feelingNumber < 3)
+            {
+                Console.WriteLine("Sorry.");
+            }
+            else if (feelingNumber == 3)
+            {
+                Console.WriteLine("OK.");
+            }            }
+            else if (feelingNumber > 3)
+            {
+                Console.WriteLine("Yay.");
+            else
+            {
+                Console.WriteLine("Sorry, we don't understand");
+            }
+        }
+    }
+}
---
+++ 0.04_Conditionals2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._04_Conditionals2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How much is in bank account?");
+            string bankAccount = Console.ReadLine();
+            int amount = Convert.ToInt32(bankAccount);
+            if (amount > 10000)
+            {
+                Console.WriteLine("Yay!");
+            }
+
+            else if (amount <= 10000)
+            {
+                Console.WriteLine("Sorry");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, we don't understand. Come back later.");
+            }
+
+        }
+    }
+}
