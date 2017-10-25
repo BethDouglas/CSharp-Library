ab51c8c61eb17b57b1b9ee553573ab102c2c473e
Beth Douglas <bethDdouglas@gmail.com>
2017-10-25 11:14:54 -0400

    Initial Commit

---
+++ 0.02_BasicTypes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._02_BasicTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte myByte = 255; //range: 0 - 255
+
+            int number1 = 15;
+
+            float myFloat = 3.14f;
+
+            double myDouble = 2.54;
+
+            decimal myDecimal = 3.14159m;
+
+            char myLetter = 'D';
+
+            string myName = "bethD";
+
+            bool myTruth = true;
+
+            //Declaration
+            // <type> <variableName> = <optional initializer>;
+            int age;
+            string country;
+            //Initialization
+            age = 22;
+            Console.WriteLine(age);       
+            age = 24;
+            Console.WriteLine(age);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ 0.02_BasicTypesChallenges/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._02_BasicTypesChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myYear = 1999;
+            int myCentury = 19;
+            string myString = "I was born in";
+            string myDecade = "XX";
+            bool myBool = false;
+            float myFloat = 3.14f;
+            double myDouble = 2.54;
+            decimal myDecimal = 1.5m;
+            string mySentance = myString + " " + myCentury + myDecade;
+            string birthYear = "1999";
+            int year = Convert.ToInt32(birthYear);
+            //Console.WriteLine(myYear);
+            //Console.WriteLine(myString);
+            //Console.WriteLine(myBool);
+            //Console.WriteLine(myFloat);
+            //Console.WriteLine(myDouble);
+            //Console.WriteLine(myDecimal);
+            //Console.WriteLine(myYear + " " + myString + " " + myBool);
+            //Console.WriteLine(myString + " " + myYear);
+            //Console.WriteLine(myString + " " + myCentury + myDecade);
+            //Console.WriteLine(mySentance);
+            Console.WriteLine("I was born in " + year);
+            Console.ReadLine();
+
+
+        }
+    }
+}
