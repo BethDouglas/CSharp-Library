bc62cd50ab087346b1ca1fba101b0f56873fecc4
Beth Douglas <bethDdouglas@gmail.com>
2017-10-25 13:08:54 -0400

    Added Strings

---
+++ 0.04_StringChallenges/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._04_StringChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bronze
+            //Currency
+            double cash = 563.32;
+            Console.WriteLine("{0:C}",cash);
+
+            //Percentage
+            double exchangeRate = 0.23;
+            Console.WriteLine("{0:#0.##%}", exchangeRate);
+            Console.WriteLine("{0:P}", exchangeRate);
+
+            //Phone Number
+            int phoneNumber = 1234567890;
+            string number1 = phoneNumber.ToString("(###)-###-####");
+            Console.WriteLine(number1);
+            Console.WriteLine("{0(###) ### - ####}", phoneNumber);
+            Console.ReadLine();
+        }
+    }
+}
