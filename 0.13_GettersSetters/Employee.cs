08ee9511d1c540548ce35962c56e9849fa748e12
Beth Douglas <bethDdouglas@gmail.com>
2017-11-01 12:03:50 -0400

    added Getters and Setters

---
+++ 0.13_GettersSetters/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._13_GettersSetters
+{
+    class Employee
+    {
+        //fields or backing fields
+        private int _id;
+        private string _workQuote;
+
+        //Properties
+        public int ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new Exception("The employee ID is invalid");
+                _id = value;
+            }
+
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                    throw new Exception("Invalid Employee Name.");
+                _name = value.Trim();
+            }
+        }
+        //Methods
+        public void SetWorkQuote(string quote)
+        {
+            this._workQuote = quote;
+        }
+        public string GetQuote()
+        {
+            return this._workQuote;
+        }
+
+    }
+}
