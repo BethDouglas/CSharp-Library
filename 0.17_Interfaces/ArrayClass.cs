53644bce869be582ebf06c89e12e76e82525184b
Beth Douglas <bethDdouglas@gmail.com>
2017-11-02 16:40:23 -0400

    Added Interfaces

---
+++ 0.17_Interfaces/ArrayClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._17_Interfaces
+{
+    class ArrayClass : IEnum, IList, ICollect
+    {
+        public void IterateOverTheCollection()
+        {
+            Console.WriteLine("You are iterating over the array");
+        }
+        public void RemoveStuff()
+        {
+            Console.WriteLine("You removed stuff from the array.");
+        }
+        public void AddStuff()
+        {
+            Console.WriteLine("You added stuff from the array.");
+        }
+        public void CheckTheCount()
+        {
+            Console.WriteLine("Checking the count.");
+        }
+        public void CheckTheCapacity()
+        {
+            Console.WriteLine("You can't check capacity on an array");
+        }
+    }
+}
---
+++ 0.17_Interfaces/ListClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._17_Interfaces
+{
+    class ListClass : IEnum, IList, ICollect
+    {
+        public void AddStuff()
+        {
+            Console.WriteLine("Add stuff to list");
+        }
+
+        public void CheckTheCapacity()
+        {
+            Console.WriteLine("Check the capacity of the list");
+        }
+
+        public void CheckTheCount()
+        {
+            Console.WriteLine("Check the count of the list");
+        }
+
+        public void IterateOverTheCollection()
+        {
+            Console.WriteLine("Iterate over the list");
+        }
+
+        public void RemoveStuff()
+        {
+            Console.WriteLine("Iterating over the list");
+        }
+    }
+}
