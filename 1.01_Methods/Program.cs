aa72a2dacd790980d0cc93471ac186eec11c7a7c
Beth Douglas <bethDdouglas@gmail.com>
2017-11-11 10:37:39 -0500

    added methods

---
+++ 1.01_Methods/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._01_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double doMath(string operation, double numOne, double numTwo)
+            {
+                switch (operation.ToLower())
+                {
+                    case "add":
+                        return numOne + numTwo;
+                    case "subtract":
+                        return numOne - numTwo;
+                    case "multiply":
+                        return numOne * numTwo;
+                    case "divide":
+                        return numOne / numTwo;
+                    case "mod":
+                        return numOne % numTwo;
+                    default:
+                        Console.WriteLine("Operation not availible.");
+                        return 0;
+                }
+            }
+            Console.WriteLine(doMath("add", 5D, 6D));
+
+            string FullName(string first, string last)
+            {
+                return first + " " + last;
+            }
+            Console.WriteLine(FullName("beth", "douglas"));
+
+            int TwoNums(int numOne, int numTwo)
+            {
+                return numOne + numTwo;
+            }
+            Console.WriteLine(TwoNums(20,6));
+        }        
+    }
+}
