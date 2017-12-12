12cc80c0b0d242b27d4683202f2fd68a060975c7
Beth Douglas <bethDdouglas@gmail.com>
2017-12-12 11:40:59 -0500

    added gg quiz

---
+++ GoldenQuiz/Hard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldenQuiz
+{
+    class Hard
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Question #1");
+            Console.WriteLine("Which of these things has NOT happened to Blanche in Chattanooga?");
+            Console.WriteLine("A: She had an encounter with Elvis in a Motel6.\n" +
+            "B: She was arrested for driving topless.\n" +
+            "C: She was with a group arrested for 'dividing into teams and greasing their hands'.\n" +
+            "D: She made friends with the Sherrif, who still writes.\n" +
+            "E: She bla bla something that's not true");
+
+
+
+
+
+        }
+    }
+}
---
+++ GoldenQuiz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoldenQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            Console.WriteLine("Hi!\n" +
+                               "What's your name?");
+            string inputName = Console.ReadLine();
+
+            Console.Clear();
+            Console.WriteLine($"Glad to meet you, {inputName}.");
+            Thread.Sleep(1000);
+            Console.WriteLine("\nChoose a level.\n" +
+                "1: Rose - Easy (Also could be called Blanche)\n" +
+                "2: Dorothy - Hard\n" +
+                "3: Sophia - Sassy\n");
+
+            //Get Value User Provided
+            int inputLevel = Int32.Parse(Console.ReadLine());
+        }
+    }
+}
